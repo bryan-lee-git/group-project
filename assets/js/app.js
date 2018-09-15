446929de50d768700f92122b136cb4cb4f8fa094
bryanlee <blee@utahsbr.edu>
2018-09-14 22:00:39 -0600

    created tuner functionality, made test html page for test display until added into app, cloned down the actual source code from github, all files located in the js folder in a folder called tuner. Also created functionality for YouTube search and display embedded videos on page via ajax api call and google developer api key. Lastly, commented and organized the landing page html so that it is easy to navigate on the backend

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,95 +1,134 @@
-function replaceSpaces(artist) {
-    return artist.replace(/\s/g, "&");
-}
+//----------------------------------------------------------------------------------------
+// MATERIALIZE FUNCTIONALITY
+//----------------------------------------------------------------------------------------
 
-$("#form-submit").on("click", function(event) {
-    event.preventDefault();
-    var userInput = $("#user-input").val().trim();
-    userInput = removeSpaces(userInput);
-    getTabs(userInput);
-});
+    // carousel functionality (Google Materialize)
 
-function fillTable(child) {
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
 
-    // console log entire child object
-    console.log(child);
+    // nav bar dropdown (Google Materialize)
 
-    // set variable to hold chords icon
-    var chords = "";
+    $(".dropdown-trigger").dropdown({hover: true});
 
-    // chords if true or if false
-    if (child.chordsPresent === true) {
-        chords = "✅";
-    } else {
-        chords = "❌";
+//----------------------------------------------------------------------------------------
+// SONGSTER API
+//----------------------------------------------------------------------------------------
+
+    // function for replacing spaces with the & in user search queries
+
+    function replaceSpaces(artist) {
+        return artist.replace(/\s/g, "&");
     }
+
+    // take in the form input value when it is submitted and run it through the getTabs function
+
+    $("#form-submit").on("click", function(event) {
+        event.preventDefault();
+        var userInput = $("#user-input").val().trim();
+        userInput = removeSpaces(userInput);
+        getTabs(userInput);
+    });
+
+    // function to fill up the data table from the getTabs resulting children
+
+    function fillTable(child) {
+
+        // console log entire child object
+        console.log(child);
+
+        // set variable to hold chords icon
+        var chords = "";
+
+        // chords if true or if false
+        if (child.chordsPresent === true) {
+            chords = "✅";
+        } else {
+            chords = "❌";
+        }
+        
+        // create an empty variable to hold tab links
+        var types = [];
+
+        // for teach item in the tab types array do this stuff
+        child.tabTypes.forEach(function(type) {
+
+            // if there is a guitar tab...
+            if (type === "TEXT_GUITAR_TAB") {
+                var tabUrl = "<a href='http://www.songsterr.com/a/wa/bestMatchForQueryString?s=" + child.title + "&a=" + child.artist.name + "&track='guitar''> Guitar Tab</a>";
+                types.push(tabUrl);
+
+            // if there is a bass tab...            
+            } else if (type === "TEXT_BASS_TAB") {
+                var tabUrl = "<a href='http://www.songsterr.com/a/wa/bestMatchForQueryString?s=" + child.title + "&a=" + child.artist.name + "&track='bass''> Bass Tab</a>";
+                types.push(tabUrl);
+            }
+
+            // if there is a player tab...
+            if (type === "PLAYER") {
+                var tabUrl = "<a href='http://www.songsterr.com/a/wa/bestMatchForQueryString?s=" + child.title + "&a=" + child.artist.name + "&track='player''> Tab Player</a>";
+                types.push(tabUrl);
+            }
+            if (type === "CHORDS") {
+                var tabUrl = "<a href='https://www.songsterr.com/a/wsa/" + child.artist.name + "-" + child.title + "-chords-s" + child.id + "'> View Chords</a>";
+                types.push(tabUrl);
+            }
+        })
+
+        // fill table with child data
+        $("tbody").append(
+            "<tr>"
+            + "<td>" + child.title + "</td>"
+            + "<td>" + types + "</td>"
+            + "<td>" + chords + "</td>"
+            + "</tr>"
+        )
+    };
+
+    // function to get list of tabs via the songsterr.com API
+
+    function getTabs(userInput) {
+
+        var queryURL = "http://www.songsterr.com/a/ra/songs/byartists.json?artists=" + userInput;
     
-    // create an empty variable to hold tab links
-    var types = [];
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(songsterr) {
+            songsterr.forEach(function(child) {
+                fillTable(child);
+            })
 
-    // for teach item in the tab types array do this stuff
-    child.tabTypes.forEach(function(type) {
+            $('#artist-search').DataTable();
+        })
+    };
 
-        // if there is a guitar tab...
-        if (type === "TEXT_GUITAR_TAB") {
-            var tabUrl = "<a href='http://www.songsterr.com/a/wa/bestMatchForQueryString?s=" + child.title + "&a=" + child.artist.name + "&track='guitar''> Guitar Tab</a>";
-            types.push(tabUrl);
+    // testing that the getTabs function works
 
-        // if there is a bass tab...            
-        } else if (type === "TEXT_BASS_TAB") {
-            var tabUrl = "<a href='http://www.songsterr.com/a/wa/bestMatchForQueryString?s=" + child.title + "&a=" + child.artist.name + "&track='bass''> Bass Tab</a>";
-            types.push(tabUrl);
-        }
+    $(document).ready( function () {
+        var search = "metallica";
+        getTabs(search);
+    });
 
-        // if there is a player tab...
-        if (type === "PLAYER") {
-            var tabUrl = "<a href='http://www.songsterr.com/a/wa/bestMatchForQueryString?s=" + child.title + "&a=" + child.artist.name + "&track='player''> Tab Player</a>";
-            types.push(tabUrl);
-        }
-        if (type === "CHORDS") {
-            var tabUrl = "<a href='https://www.songsterr.com/a/wsa/" + child.artist.name + "-" + child.title + "-chords-s" + child.id + "'> View Chords</a>";
-            types.push(tabUrl);
-        }
-    })
+//----------------------------------------------------------------------------------------
+// YOUTUBE API
+//----------------------------------------------------------------------------------------
 
-    // fill table with child data
-    $("tbody").append(
-        "<tr>"
-        + "<td>" + child.title + "</td>"
-        + "<td>" + types + "</td>"
-        + "<td>" + chords + "</td>"
-        + "</tr>"
-    )
-};
+// key: AIzaSyDEN3-Xo9I-Ycjy-cTlOygMnHB3p5ZhVg4
+// function to get list of tabs via the songsterr.com API
 
-function getTabs(userInput) {
+function getVideos(userInput) {
 
-    var queryURL = "http://www.songsterr.com/a/ra/songs/byartists.json?artists=" + userInput;
-  
+    var queryURL = "GET https://www.googleapis.com/youtube/v3/search=metallica";
+
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(songsterr) {
-
-        songsterr.forEach(function(child) {
-            fillTable(child);
+    }).then(function(videoData) {
+        videoData.forEach(function(child) {
+            console.log(child);
         })
-
-        $('#artist-search').DataTable();
     })
 };
-
-$(document).ready( function () {
-    var search = "metallica";
-    getTabs(search);
-});
-
-//js for carousel
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-  });
-
-//js for dropdown
-$(".dropdown-trigger").dropdown({hover: true});
-
