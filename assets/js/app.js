dd2ee8e57876a94a799723ce57b7e8ede9d8b237
bryanlee <blee@utahsbr.edu>
2018-09-17 22:13:46 -0600

    fixed all remaining merge conflicts. Moved Jims firebase code to the bottom of the js file so that it would not prevent the rest of the page from loading

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,92 +1,3 @@
-//---------------------------------------------------------------------------
-// Firebase Config 
-//---------------------------------------------------------------------------
-
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDF16YKI3mYgmTkUOQ09dANHOIVjMsFcJk",
-    authDomain: "group-project-1-3b1fb.firebaseapp.com",
-    databaseURL: "https://group-project-1-3b1fb.firebaseio.com",
-    projectId: "group-project-1-3b1fb",
-    storageBucket: "group-project-1-3b1fb.appspot.com",
-    messagingSenderId: "502946428711"
-  };
-  firebase.initializeApp(config);
-
-  var database = firebase(database);
-
-//---------------------------------------------------------------------------
-// Firebase Authentication Config with UI
-//---------------------------------------------------------------------------
-
-var uiConfig = {
-    signInSuccessUrl: 'https://bryan-lee-git.github.io/group-project/index.html',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-      //firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-    ],
-    // tosUrl and privacyPolicyUrl accept either url string or a callback
-    // function.
-    // Terms of service url/callback.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url/callback.
-    privacyPolicyUrl: function() {
-      window.location.assign('<your-privacy-policy-url>');
-    }
-  };
-
-  // Initialize the FirebaseUI Widget using Firebase.
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  // The start method will wait until the DOM is loaded.
-  ui.start('#firebaseui-auth-container', uiConfig);
-
-
-  initApp = function() {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var uid = user.uid;
-        var phoneNumber = user.phoneNumber;
-        var providerData = user.providerData;
-
-        $("#user").text(user.displayName);
-        $("#user-button").hide();
-        $("#menu").show();
-        $(".user-tabs").show();
-        $("#logout").on("click", function(){
-            user.signout();
-        })
-       
-      } else {
-        // User is signed out.
-        $("#user").hide();
-        $("#user-button").show();
-        $("#menu").hide();
-        $(".user-tabs").hide();
-        $("#user-button", "#carousel-row").on("click", function(){
-            initApp();
-         });
-      }
-    }, function(error) {
-      console.log(error);
-    });
-  };
-
-  window.addEventListener('load', function() {
-    initApp()
-  });
-
-
 //----------------------------------------------------------------------------------------
 // Tabs Click Display/Hide Functionality
 //----------------------------------------------------------------------------------------
@@ -232,3 +143,92 @@ $(".dropdown-trigger, #tab-dropdown, #videos-dropdown, #tuner-dropdown, #metrono
     event.preventDefault();
     $("#dropdown1").fadeToggle(500);
 })
+
+//---------------------------------------------------------------------------
+// Firebase Config 
+//---------------------------------------------------------------------------
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDF16YKI3mYgmTkUOQ09dANHOIVjMsFcJk",
+    authDomain: "group-project-1-3b1fb.firebaseapp.com",
+    databaseURL: "https://group-project-1-3b1fb.firebaseio.com",
+    projectId: "group-project-1-3b1fb",
+    storageBucket: "group-project-1-3b1fb.appspot.com",
+    messagingSenderId: "502946428711"
+  };
+  
+  firebase.initializeApp(config);
+
+  var database = firebase(database);
+
+//---------------------------------------------------------------------------
+// Firebase Authentication Config with UI
+//---------------------------------------------------------------------------
+
+var uiConfig = {
+    signInSuccessUrl: 'https://bryan-lee-git.github.io/group-project/index.html',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+      //firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+    ],
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url/callback.
+    privacyPolicyUrl: function() {
+      window.location.assign('<your-privacy-policy-url>');
+    }
+  };
+
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
+
+
+  initApp = function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var uid = user.uid;
+        var phoneNumber = user.phoneNumber;
+        var providerData = user.providerData;
+
+        $("#user").text(user.displayName);
+        $("#user-button").hide();
+        $("#menu").show();
+        $(".user-tabs").show();
+        $("#logout").on("click", function(){
+            user.signout();
+        })
+       
+      } else {
+        // User is signed out.
+        $("#user").hide();
+        $("#user-button").show();
+        $("#menu").hide();
+        $(".user-tabs").hide();
+        $("#user-button", "#carousel-row").on("click", function(){
+            initApp();
+         });
+      }
+    }, function(error) {
+      console.log(error);
+    });
+  };
+
+  window.addEventListener('load', function() {
+    initApp()
+  });
