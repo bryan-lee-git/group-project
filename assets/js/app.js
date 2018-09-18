d8ec51c54d99317231c085e530b3abbe9b44fae1
unknown <jamesschlader@yahoo.com>
2018-09-18 11:21:53 -0600

    Rebuild the structure of the app to accomodate the Firebase auth. It functions, although it is not moving straight to the sign-in interface after selecting email. Console.log shows that the user info is loaded, it just doesn't make the final step to sign in without another click on the screen. Weird.

--- assets/js/app.js
+++ assets/js/app.js
@@ -160,39 +160,11 @@ var config = {
   
   firebase.initializeApp(config);
 
-  var database = firebase(database);
+  var database = firebase.database();
 
 //---------------------------------------------------------------------------
 // Firebase Authentication Config with UI
 //---------------------------------------------------------------------------
-
-var uiConfig = {
-    signInSuccessUrl: 'https://bryan-lee-git.github.io/group-project/index.html',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-      //firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-    ],
-    // tosUrl and privacyPolicyUrl accept either url string or a callback
-    // function.
-    // Terms of service url/callback.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url/callback.
-    privacyPolicyUrl: function() {
-      window.location.assign('<your-privacy-policy-url>');
-    }
-  };
-
-  // Initialize the FirebaseUI Widget using Firebase.
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  // The start method will wait until the DOM is loaded.
-  ui.start('#firebaseui-auth-container', uiConfig);
-
 
   initApp = function() {
     firebase.auth().onAuthStateChanged(function(user) {
@@ -207,22 +179,12 @@ var uiConfig = {
         var providerData = user.providerData;
 
         $("#user").text(user.displayName);
-        $("#user-button").hide();
-        $("#menu").show();
-        $(".user-tabs").show();
-        $("#logout").on("click", function(){
-            user.signout();
-        })
+       
        
       } else {
         // User is signed out.
-        $("#user").hide();
-        $("#user-button").show();
-        $("#menu").hide();
-        $(".user-tabs").hide();
-        $("#user-button", "#carousel-row").on("click", function(){
-            initApp();
-         });
+       
+        window.location.assign('landing.html')
       }
     }, function(error) {
       console.log(error);
@@ -232,3 +194,7 @@ var uiConfig = {
   window.addEventListener('load', function() {
     initApp()
   });
+
+  $("#logout").on("click", function(){
+    firebase.auth().signOut();
+})
